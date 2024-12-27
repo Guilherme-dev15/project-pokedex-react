9e586905a600f9e30ddf2ef3bc1ff6450852887e
Guilherme Anjos <guilherme.macedo1598@gmail.com>
2024-12-27 11:13:02 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project-pokedex-react",
+  base: "/project-pokedex-react", // Nome do seu repositório no GitHub
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
