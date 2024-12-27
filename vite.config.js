78db8b28b71acb76de92a20665eabcfeb2ee0afd
Guilherme Anjos <guilherme.macedo1598@gmail.com>
2024-12-27 11:24:04 -0300

    vite config update

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-
 export default defineConfig({
   plugins: [react()],
-  base: './project-pokedex-react', 
-  test: {
-    include: ['src/__tests__/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // Caminho dos testes
-    exclude: ['**/node_modules/**', '**/dist/**'], // Exclui node_modules e dist
-    watchExclude: ['**/node_modules/**', '**/dist/**'], // Também exclui esses ao assistir os arquivos
-    setupFiles: ['./src/setupTests.js'], // Caminho para o arquivo de configuração de testes
-  },
+  base: '/project-pokedex-react/',  // Substitua pelo nome do seu repositório
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
   },
